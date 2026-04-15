2834707b4739c17aebd17f4d4e82e28ca2da0a61
FRANCISDAX <149949315+FRANCISDAX@users.noreply.github.com>
2026-04-15 15:25:21 -0500

    Add Usuario Mapper

--- src/main/java/com/cibersoftcys/canchawebpro/Usuarios/aplicacion/mapeadores/UsuarioMapperApp.java
+++ src/main/java/com/cibersoftcys/canchawebpro/Usuarios/aplicacion/mapeadores/UsuarioMapperApp.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 import com.cibersoftcys.canchawebpro.Usuarios.aplicacion.dtos.UsuarioRequest;
 import com.cibersoftcys.canchawebpro.Usuarios.aplicacion.dtos.UsuarioResponse;
 import com.cibersoftcys.canchawebpro.Usuarios.dominio.modelos.Usuario;
+import com.cibersoftcys.canchawebpro.Usuarios.dominio.modelos.enums.TipoUsuario;
+import com.cibersoftcys.canchawebpro.Usuarios.dominio.modelos.valueObject.EmailUsuario;
 import com.cibersoftcys.canchawebpro.Usuarios.dominio.modelos.valueObject.NombreUsuario;
+import com.cibersoftcys.canchawebpro.Usuarios.dominio.modelos.valueObject.PasswordUsuario;
 
 @Component
 public class UsuarioMapperApp {
@@ -17,9 +20,10 @@ public class UsuarioMapperApp {
         UsuarioResponse response = new UsuarioResponse();
         response.setId(usuario.getId());
         response.setNombre(usuario.getNombre().getValor());
-        response.setEmail(usuario.getEmail());
+        response.setEmail(usuario.getEmail().getValor());
         response.setTelefono(usuario.getTelefono());
         response.setTipo(usuario.getTipo().name());
+        response.setFechaRegistro(usuario.getFechaRegistro().toString());
         return response;
     }
 
@@ -27,13 +31,19 @@ public class UsuarioMapperApp {
     public Usuario toDomain(UsuarioRequest request) {
         if(request == null) return null;
 
-        NombreUsuario nombre = new NombreUsuario(request.getNombre());
-        return new Usuario(
-            nombre,
-            request.getEmail(),
+        Usuario usuario = new Usuario(
+            new NombreUsuario(request.getNombre()),
+            EmailUsuario.crear(request.getEmail()),
             request.getTelefono(),
-            null
+            TipoUsuario.CLIENTE
         );
+
+        // 🔐 asignar password
+        usuario.asignarPassword(
+            PasswordUsuario.crear(request.getPassword())
+        );
+
+        return usuario;
     }
     
 }

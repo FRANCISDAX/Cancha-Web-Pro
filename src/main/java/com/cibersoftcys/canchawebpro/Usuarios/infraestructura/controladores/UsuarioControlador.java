e2502e2e7e4d311241292a27df2c1d7d1cb50f97
FRANCISDAX <149949315+FRANCISDAX@users.noreply.github.com>
2026-04-15 15:44:05 -0500

    Fix Controlador Usuario

--- src/main/java/com/cibersoftcys/canchawebpro/Usuarios/infraestructura/controladores/UsuarioControlador.java
+++ src/main/java/com/cibersoftcys/canchawebpro/Usuarios/infraestructura/controladores/UsuarioControlador.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,6 @@ import com.cibersoftcys.canchawebpro.Usuarios.aplicacion.dtos.UsuarioResponse;
 import com.cibersoftcys.canchawebpro.Usuarios.dominio.puertos.entrada.UsuarioServicioPuerto;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 

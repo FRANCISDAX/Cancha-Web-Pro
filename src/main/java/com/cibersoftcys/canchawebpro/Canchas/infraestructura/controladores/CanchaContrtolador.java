d3c7a7096f0c498568dcf3e2fb06b3049eafa4fa
FRANCISDAX <149949315+FRANCISDAX@users.noreply.github.com>
2026-04-15 19:53:25 -0500

    Add CrossOrigin

--- src/main/java/com/cibersoftcys/canchawebpro/Canchas/infraestructura/controladores/CanchaContrtolador.java
+++ src/main/java/com/cibersoftcys/canchawebpro/Canchas/infraestructura/controladores/CanchaContrtolador.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/canchas")
 @RequiredArgsConstructor
